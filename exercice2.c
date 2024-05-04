2e52a2ee2097adfcd7968e403f2b7eb63f78c031
safidi <safidi@mx>
2024-05-04 03:46:07 +0200

    exercice 2 : produit de deux entier

---
+++ exercice2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	float a,b;
+	printf("entrer a:");
+	scanf("%f",&a);
+	printf("entrer b:");
+	scanf("%f",&b);
+	printf("le produit  est : %f",a*b);
+	
+	return 0;
+}
